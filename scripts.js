08b21fadb6056b9fb404502f2e4b270a9bd37b5d
Dyushes <nohtimore@gmail.com>
2023-11-28 00:03:35 +0500

    музыка

--- scripts.js
+++ scripts.js
@@ -2,15 +2,15 @@ var current_image = 0
 
 var images = document.getElementsByClassName('img-wrapper')
 
-function next_image(){
-    if (current_image + 1 > images.length-1){
+function next_image() {
+    if (current_image + 1 > images.length - 1) {
         console.log('Stop')
         current_image = 0
     } else {
-        current_image+=1
+        current_image += 1
     }
 
-    for (let image of images){
+    for (let image of images) {
         image.classList.add('disabled')
         image.classList.remove('active_image')
 
@@ -21,15 +21,15 @@ function next_image(){
 }
 
 
-function prev_image(){
-    if (current_image - 1 < 0){
+function prev_image() {
+    if (current_image - 1 < 0) {
         console.log('Stop')
         current_image = images.length - 1
     } else {
-        current_image-=1
+        current_image -= 1
     }
 
-    for (let image of images){
+    for (let image of images) {
         console.log(image)
         image.classList.add('disabled')
         image.classList.remove('active_image')
@@ -37,4 +37,8 @@ function prev_image(){
 
     images[current_image].classList.remove('disabled')
     images[current_image].classList.add('active_image')
+}
+
+document.onclick = ()=>{
+    document.getElementById('audio').play()
 }
